50932c7e24ae36946e5e82155706da6b712a0eda
gitdarwinfredy <agarcia1007@une.net.co>
2022-04-21 15:34:34 -0500

    modal

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ app.use(express.json());
 const dotenv = require('dotenv');
 dotenv.config({path:'./env/.env'});
 
+
 //4- Creamos el directorio public
-app.use('/resources', express.static('public'));
-app.use('/resources', express.static(__dirname + '/public'));
+// app.use('/resources', express.static('public'));
+// app.use('/resources', express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 console.log(__dirname);
 
@@ -37,17 +39,78 @@ const connection = require('./database/db');
 
 //9- Estableciendo Rutas
 
-    app.get('/', (req, res)=>{
-        res.render('index.ejs',{msg:'ESTOS ES UN MENSAJE DESDE NODE'});
-    });
-
     app.get('/login',( req, res)=>{
         res.render('login.ejs');
     });
 
+     app.get('/editar',( req, res)=>{
+        connection.query('SELECT * FROM users' , async(error,results) =>{
+            if(error){
+                throw error;
+            }else{
+                data = JSON.stringify(results);
+                 res.render('editar.ejs',{
+                    results: results,
+                    data
+                });
+            } 
+        })
+     });
+
+     app.get('/editarB',( req, res)=>{
+        connection.query('SELECT * FROM users' , async(error,results) =>{
+            if(error){
+                throw error;
+            }else{
+                data = JSON.stringify(results);
+                 res.render('editarB.ejs',{
+                    results: results,
+                    data
+                });
+            } 
+        })
+     });
+
+     app.get('/editarB1',( req, res)=>{
+        connection.query('SELECT * FROM users limit 10 ' , async(error,results) =>{
+            if(error){
+                throw error;
+            }else{
+                data = JSON.stringify(results);
+                 res.render('editarB.ejs',{
+                    results: results,
+                    data
+                });
+            } 
+        })
+     });
+
+     app.get('/editarB2',( req, res)=>{
+        connection.query('SELECT * FROM users limit 10, 10 ' , async(error,results) =>{
+            if(error){
+                throw error;
+            }else{
+                data = JSON.stringify(results);
+                 res.render('editarB.ejs',{
+                    results: results,
+                    data
+                });
+            } 
+        })
+     });
+
+
     app.get('/register',(req, res)=>{
         res.render('register.ejs');
     });
+
+   
+
+    /* app.get('/consulta',(req, res)=>{
+        res.render('consulta.ejs');
+    }); */
+
+   
 
 //10- registracion
 app.post('/register', async (req ,res)=>{
@@ -77,32 +140,158 @@ app.post('/register', async (req ,res)=>{
                         }
                     })
 
-})
+});
 
 //11- autenticacion
-app.get('/auth', async (req, res)=>{
+app.post('/auth', async (req, res)=>{
     const user = req.body.user;
     const pass = req.body.pass;
-    let passwordHaash = await bcrypts.hash(pass, 8);
+    let passwordHaash = await bcryptjs.hash(pass, 8);
     if(user && pass){
     connection.query('SELECT * FROM users WHERE user=?', [user], async(error,results)=>{
-     if(results.length == 0 || !(await bcrypts.compare(pass, results[0].pass))){
-        res.send('usuario yo password invalido');
+     if(results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass))){
+        res.render('login.ejs', {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "Datos Ingresados Incorrectos",
+            alertIcon: 'error',
+            showConfirmButton: true,
+            timer: false,
+            ruta: 'login'
+        });
         }else{
-            res.send('usuario logueado');
-        }
-    
+            req.session.logeding = true;
+            req.session.name= results[0].name;
+            req.session.rol= results[0].rol;
+            res.render('login.ejs', {
+                alert: true,
+                alertTitle:"Felicitaciones",
+                alertMessage:"Usuario y password correcto",
+                alertIcon: 'success',
+                showConfirmButton: false,
+                timer: 2000,
+                ruta:''
+            });
+        }    
     
     })
-  }
-})
+  }else{
+    res.render('login.ejs',{
+        alert: true,
+        alertTitle: "Error",
+        alertMessage:"No ha ingresado Usuario y Password",
+        alertIcon: 'error',
+        showConfirmButton: true,
+        timer:false,
+        ruta:'login'
+    
+    });
+ }
+});
+
+//12 - logueo
+app.get('/', (req, res)=>{
+        if(req.session.logeding){        
+        res.render('index.ejs',{                       
+            login: true,
+            name: req.session.name,
+            rol: req.session.rol
+            });
+            }else{                
+                res.render('index.ejs',{
+                login: false,
+                name: 'Debe iniciar session',
+                rol: ''
+                });
+             }            
+         
+   });
+
+//13 - Destruir la sesssion
+app.get('/logout', (req, res)=>{
+    req.session.destroy(()=>{
+        res.redirect('/');
+    })
+});
 
 
+ 
+//14 - consulta -Si el usuario existe se direccona a editar.ejs
+app.post('/consultar', (req , res)=>{
+
+    const id = req.body.id;
+    const user = req.body.user;
+    if(user){
+        connection.query('SELECT * FROM users WHERE user=?', [user], async(error, results)=>{
+            if(results.length == 0 ){
+            res.send("usuario incorrecto");
+        
+            }else{
+                                
+               res.render('edit.ejs',{
+                   results : results[0]
+                   
+               });
+                
+
+            }
+        })
+        }
+    });
+
+//-15 Editar si el usuario existe en params se direccion a save.ejs y envia results
+app.get('/edit/:user/',(req, res)=>{
+    
+    const user = req.params.user;
+    connection.query('SELECT * FROM users WHERE user=?',[user], async(error,results) =>{
+        if(error){
+            throw error;
+        }else{ 
+            
+            res.render('edit.ejs',{
+                results: results[0]
+            });
+            
+        } 
+    })
+   });
+
+//-16 Actualizar
+app.post('/update', async (req, res) => {
+        
+        const user = req.body.user;
+        const name = req.body.name;
+        const rol  = req.body.rol;
+        const pass = req.body.pass;
+        let passwordHaash = await bcryptjs.hash(pass, 8);
+
+ 
+
+        connection.query('UPDATE users SET ? WHERE user=?', [{ name: name, rol: rol, pass: passwordHaash }, user], async (error, results) => {
+            if (error) {
+                throw error;
+            } else {
+                res.redirect('/');
+            }
+        });
+
+    });
+
+//-17 Eliminar
+app.get('/eliminar/:user', (req, res)=>{
+    const user = req.params.user;
+    connection.query('DELETE FROM users WHERE user = ?', [user], (error,results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/');
+        }
+    })
+
+});
 
 
-
-
-app.listen(3000, (req, res)=>{
+app.listen('3000', (req, res)=>{
     console.log('SERVER RUNNIG EN http://localhost:3000');
-})
+});
 
